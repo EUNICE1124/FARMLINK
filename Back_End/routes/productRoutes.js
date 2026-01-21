0198b57423eca09fd82bed9df4cd73b3a39a0be8
Tchouela eunice <tchouelaeunice@gmail.com>
2026-01-21 08:24:47 +0100

    done with the cartpage backend and connections

--- Back_End/routes/productRoutes.js
+++ Back_End/routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 router.get('/products', productController.getProducts);
-router.post('/cart/add', productController.addToCart); 
+router.post('/cart/add', productController.addToCart);
 router.post('/save', productController.saveProduct);
 
 module.exports = router;
